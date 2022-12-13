2921e1a7341aa13df82e06452cb0b825b2319c8c
Denis <d.cochew@yandex.ru>
2022-12-13 07:22:32 +0700

    z_4

--- lesson_4/z_4.py
+++ lesson_4/z_4.py
@@ -6,6 +6,7 @@
 # Результат: [23, 1, 3, 10, 4, 11]
 
 
-first_list =  [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+first_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 res_list = [el for el in first_list if first_list.count(el) == 1]
-print = (res_list)
+print(res_list)
+
